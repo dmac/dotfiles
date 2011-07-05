4294f4c7bb720ab62ef9f35f222c5e00c7689469
Daniel MacDougall <dmacdougall@gmail.com>
2011-07-05 14:36:22 -0700

    Use bash script instead of ruby script for linking dotfiles

---
+++ link_dotfiles.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+DOTDIR=`pwd`
+cd $DOTDIR
+echo "Linking files in ${DOTDIR}"
+for file in .*
+do
+  if [[ $file != . ]] &&
+     [[ $file != .. ]] &&
+     [[ $file != .git ]]; then
+    rm -fr ~/$file
+    ln -Fs $DOTDIR/$file ~/$file
+    echo $file
+  fi
+done
