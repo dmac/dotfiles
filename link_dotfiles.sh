fc7f3d0bf0ae205f2d5284fe034ba578005be3a3
Daniel MacDougall <dmacdougall@gmail.com>
2011-07-20 14:22:18 -0700

    compatibility with older versions of bash: [ instead of [[

--- link_dotfiles.sh
+++ link_dotfiles.sh
@@ -5,9 +5,9 @@ cd $DOTDIR
 echo "Linking files in ${DOTDIR}"
 for file in .*
 do
-  if [[ $file != . ]] &&
-     [[ $file != .. ]] &&
-     [[ $file != .git ]]; then
+  if [ $file != . ] &&
+     [ $file != .. ] &&
+     [ $file != .git ]; then
     rm -fr ~/$file
     ln -Fs $DOTDIR/$file ~/$file
     echo $file
