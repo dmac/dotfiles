e2353d32e619fb1ca10b711ee353869ab22caf1b
Daniel MacDougall <dmacdougall@gmail.com>
2013-09-02 10:48:20 -0700

    Emacs: Customize haskell tab indentation behavior

--- .emacs.d/custom.el
+++ .emacs.d/custom.el
@@ -4,7 +4,8 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(custom-enabled-themes (quote (base16-default)))
- '(custom-safe-themes t))
+ '(custom-safe-themes t)
+ '(haskell-indentation-cycle-warn nil))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
