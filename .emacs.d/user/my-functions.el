aa2b7ccac645ee1dcd891c52c4ef8c7f61d7af8f
Daniel MacDougall <dmacdougall@gmail.com>
2014-07-23 21:10:27 -0700

    [emacs] Add copy-pasted function for undoing buffer reverts

--- .emacs.d/user/my-functions.el
+++ .emacs.d/user/my-functions.el
@@ -89,3 +89,14 @@ but doesn't treat single semicolons as right-hand-side comments."
     ;; position after the indentation.  Else stay at same point in text.
     (if (> (- (point-max) pos) (point))
         (goto-char (- (point-max) pos)))))
+
+;; TODO: This should be tested before it's used:
+;; (setq revert-buffer-function 'revert-buffer-keep-undo)
+(defun revert-buffer-keep-undo (&rest -)
+  "Revert buffer but keep undo history."
+  (interactive)
+  (let ((inhibit-read-only t))
+    (erase-buffer)
+    (insert-file-contents (buffer-file-name))
+    (set-visited-file-modtime (visited-file-modtime))
+    (set-buffer-modified-p nil)))
