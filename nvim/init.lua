e7c412574aea37201bbf0fc588d0bfd9bc75133c
Daniel MacDougall <dmacdougall@gmail.com>
2025-08-14 11:07:02 -0700

    [vim] Update deprecated diagnostic sign customization

--- nvim/init.lua
+++ nvim/init.lua
@@ -311,10 +311,17 @@ vim.api.nvim_create_autocmd('LspAttach', {
         end)
     end,
 })
-vim.fn.sign_define("DiagnosticSignError", { text = "•", texthl = "DiagnosticSignError" })
-vim.fn.sign_define("DiagnosticSignWarn",  { text = "•", texthl = "DiagnosticSignWarn" })
-vim.fn.sign_define("DiagnosticSignHint",  { text = "•", texthl = "DiagnosticSignHint" })
-vim.fn.sign_define("DiagnosticSignInfo",  { text = "•", texthl = "DiagnosticSignInfo" })
+vim.diagnostic.config({
+    virtual_text = true,
+    signs = {
+        text = {
+            [vim.diagnostic.severity.ERROR] = "•",
+            [vim.diagnostic.severity.WARN] = "•",
+            [vim.diagnostic.severity.HINT] = "•",
+            [vim.diagnostic.severity.INFO] = "•",
+        },
+    }
+})
 lspconfig.gopls.setup({
     settings = {
         gopls = {
