fe6cf1715c6cb5617dfdf7d07613eab6fc6a6040
Daniel MacDougall <dmacdougall@gmail.com>
2025-08-14 11:07:12 -0700

    [vim] Disable formatprg for Go
    This was using gofmt for comment reflowing (e.g., 'gqap'), which I don't want.

--- nvim/init.lua
+++ nvim/init.lua
@@ -166,6 +166,7 @@ vim.api.nvim_create_autocmd({"FileType"}, {
     group = "go",
     pattern = "go",
     callback = function()
+        vim.opt_local.formatprg = ''
         vim.opt_local.tabstop = 8
         vim.opt_local.softtabstop = 8
         vim.opt_local.shiftwidth = 8
